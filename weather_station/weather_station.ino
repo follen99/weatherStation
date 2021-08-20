8e169455c9ede885b875e6d7dd81eef5d4386bfc
Giuliano Ranauro <giulianoranauroiphone1@gmail.com>
2021-08-20 22:54:44 +0200

    Code 1.0

--- weather_station/weather_station.ino
+++ weather_station/weather_station.ino
@@ -1,55 +1,144 @@
-#include "DHT.h"
 
+
+// ###################### SENSORS VARIABLES ######################
+#include "DHT.h"
 #define DHTPIN 1     // Digital pin connected to the DHT sensor
-#define PHOTOPIN A0     // Digital pin connected to the DHT sensor
+//#define PHOTOPIN A0     // Digital pin connected to the DHT sensor
+#define WINDPIN A0
+#define DHTPIN 2
 
 #define DHTTYPE DHT11   // DHT 11
 
-DHT dht(2, DHTTYPE);
+DHT dht(DHTPIN, DHTTYPE);
+
+// ###################### WEBSITE VARIABLES ######################
+//#include <WiFi.h>  NOT WORKING
+#include <ESP8266WiFi.h> . //WORKING
+#include "ThingSpeak.h"
+
+// ############ ATTENZIONE DATI DA NON CARICARE SU GITHUB ###############
+WiFiClient client;                                //WiFi connection details
+char ssid[] = "SSID";                 //WiFi Name
+char pass[] = "PASSWORD";   
+
+unsigned long myChannelNumber = channel_number;          //Thingspeak channel number
+const char * myWriteAPIKey = "WRITE_API_KEY"; 
+
+
+// ###################### MEASUREMENTS VARIABLES ######################
+int temp = 0;
+int hum = 0;
+int percTemp = 0;
+int windSpeed = 0;
+
+
+//##################### TIMER VARIABLES ############################
+long lastTime = 0;  
+long timerDelay = 30000; //time passed between every website update (30 secs)
+
+
+
+bool flag = true;
 
 void setup() {
-  Serial.begin(9600);
+  Serial.begin(115200);
+  Serial.println("\n\n");
 
-
+  pinMode(WINDPIN, INPUT);
+  
   dht.begin();
 }
 
-void loop() {
-  // Wait a few seconds between measurements.
-  delay(2000);
+void recTemp(){
+  temp = dht.readTemperature();   //reading temperature
+}
 
+void recHum(){
+  hum = dht.readHumidity();       //reading humidity
+}
 
-  // Reading temperature or humidity takes about 250 milliseconds!
-  // Sensor readings may also be up to 2 seconds 'old' (its a very slow sensor)
-  float h = dht.readHumidity();
-  // Read temperature as Celsius (the default)
-  float t = dht.readTemperature();
-  // Read temperature as Fahrenheit (isFahrenheit = true)
-  float f = dht.readTemperature(true);
+void recPercTemp(){
+  percTemp = dht.computeHeatIndex(temp, hum, false); //computing perceived temperature
+}
 
-  // Check if any reads failed and exit early (to try again).
-  if (isnan(h) || isnan(t) || isnan(f)) {
+bool checkDHT11(){
+  if (isnan(temp) || isnan(hum) || isnan(percTemp)) {
     Serial.println(F("Failed to read from DHT sensor!"));
-    return;
+    return false;
+  }
+  return true;
+}
+
+void recWindSpeed(){
+  float avg = 0;
+
+  for(int i = 0; i < 10000 ; i++){
+    avg+= analogRead(WINDPIN);
+    //delay(50);
   }
 
-  // Compute heat index in Fahrenheit (the default)
-  float hif = dht.computeHeatIndex(f, h);
-  // Compute heat index in Celsius (isFahreheit = false)
-  float hic = dht.computeHeatIndex(t, h, false);
-
-  /**
-  h = humidity
-  t = temp c
-  f = temp f
-  hic = temp percepita c
-  hif = temp percepita f
-  */
+  windSpeed = avg/10000;
+}
 
 
-  /** MIN = 0, MAX = 1024*/
-  int photoRes = analogRead(PHOTOPIN);
-  Serial.print("Photoresistor: ");
-  Serial.println(photoRes);
 
+void updateThingSpeak ()                                             //Function to post data to Thingspeak
+{
+  ThingSpeak.setField(1, temp);
+  ThingSpeak.setField(2, hum);
+  ThingSpeak.setField(3, percTemp);
+  ThingSpeak.setField(4, windSpeed);
+  
+  int x = ThingSpeak.writeFields(myChannelNumber, myWriteAPIKey);
+  if(x == 200)
+  {
+    Serial.println("Channel update successful.");
+  }
+  else
+  {
+    Serial.println("Problem updating channel. HTTP error code " + String(x));
+  }
+}
+
+
+void loop() {
+  if ((millis() - lastTime) > timerDelay) {
+    // ############ WIFI OPS ########
+    WiFi.begin(ssid, pass);                                            //Connect to WiFi network
+    Serial.print("Starting");
+    while (WiFi.status() != WL_CONNECTED)
+    {
+      delay(500);
+      Serial.print(".");
+    }
+  
+    Serial.println();
+  
+    Serial.print("Connected, IP address: ");
+    Serial.println(WiFi.localIP());
+  
+  
+    // ############ THINGSPEAK OPS ########
+    ThingSpeak.begin(client);                                          //Initialise ThingSpeak
+    delay(10000);   //wait 10 secs
+  
+
+    recWindSpeed();
+    
+    recTemp();
+    recHum();
+    recPercTemp();
+  
+    //check if sensors are ok
+    if(checkDHT11()){
+      Serial.println("Sending data...");
+      updateThingSpeak();
+    
+      Serial.println("Sended");
+    }else{
+      Serial.println("Failed reading sensors.\nWebsite data not updated.");
+    }
+
+    lastTime = millis();
+  }
 }
